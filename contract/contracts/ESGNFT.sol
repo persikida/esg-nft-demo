8eba21603592b7744f182efbd3a39a4af9baf4e4
PERSIKIDA <mboidadaev@gmail.com>
2025-07-15 09:09:10 +0200

    Add ESGNFT implementation

---
+++ contract/contracts/ESGNFT.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract ESGNFT is ERC721URIStorage {
+    uint256 public tokenCounter;
+
+    constructor() ERC721("ESGNFT", "ESG") {
+        tokenCounter = 0;
+    }
+
+    function mintNFT(address recipient, string memory tokenURI) public returns (uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(recipient, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        tokenCounter += 1;
+        return newTokenId;
+    }
+}
+
